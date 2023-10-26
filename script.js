f826bf159cd8b9c4a8fcf34168f7b7ebac055076
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-26 12:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+const table = document.querySelector("table");
+const tr = document.createElement("tr");
+const td = document.createElement("td");
+let total = 0;
 const getSum = () => {
 //Add your code here
+
+	total = 0;
+	 const prices = document.querySelectorAll(".price");
+
+    prices.forEach((price) => {
+        total += parseInt(price.textContent);
+    });
+
+    // Clear any previous total in the table
+    td.textContent = "";
+    
+    // Display the updated total in the table cell
+    td.textContent = total;
+    tr.appendChild(td);
+
+    // If a total row already exists, remove it before adding the new one
+    const existingTotalRow = table.querySelector(".total-row");
+    if (existingTotalRow) {
+        table.removeChild(existingTotalRow);
+    }
+
+    tr.className = "total-row";
+    table.appendChild(tr);
   
 };
 
